79de6d125a98a44b088e33a6f13424776b204aac
Karen L. Pedri <klp203@ilab1.cs.rutgers.edu>
2024-02-06 17:34:08 -0500

    Created initial files, and added necessary macros/prototypes to mymalloc.h q

---
+++ README.txt
@@ -0,0 +1,13 @@
+MyMalloc - CS214 P1
+
+Members:
+Karen Pedri - klp203
+Richard Li - rl902
+
+Test Plans
+-----------
+
+
+Test Programs Description
+--------------------------
+
---
+++ mymalloc.h
@@ -0,0 +1,12 @@
+// Macros and function prototypes as defined in project instructions
+#ifndef _MYMALLOC_H
+#define _MYMALLOC_H
+
+#define malloc(s) mymalloc(s, __FILE__, __LINE__)
+#define free(p) myfree(p, __FILE__, __LINE__)
+
+void *mymalloc(size_t size, char *file, int line);
+void myfree(void *ptr, char *file, int line);
+
+#endif
+
